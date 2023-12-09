265e2018975ac656d048f183efc3b84902f00cc3
Deliaml10 <114805861+Deliaml10@users.noreply.github.com>
2023-12-09 12:52:21 +0100

    cambios victor

--- src/app.js
+++ src/app.js
@@ -1,32 +1,42 @@
 import express from 'express';
-import mustacheExpress from 'mustache-express'; 
-import { __dirname } from './dirname';
-import {router} from './getRouter';
-import {fileURLToPath} from 'url';
-import {bodyParser} from 'body-parser'; 
+import mustacheExpress from 'mustache-express';
+import bodyParser from 'body-parser';
+import teamRouter from './teamRouter.js';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
 
+// Obtener el directorio actual (usando ESM)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 //pongo la pagina a escuchar peticiones en el puerto 3000
-const app = express(); 
-app.listen(3000, () => {console.log('El servidor se encuentra en el puerto 3000')} ); 
+// Definir puerto
+const port = process.env.PORT || 3000;
 
-//Configuro mustache 
-app.set('views', join(__dirname + '/../views'));
-app.set('view engine', 'mustache');
-app.engine('mustache', mustacheExpress());
+const app = express();
+app.listen(port, () => {
+  console.log("El servidor se encuentra en el puerto 3000");
+});
+dirname(__filename);
 
-//Sirve para permitir a node analizar datos de formularios cuando está en true 
+
+// Agregar router
+app.use("/", teamRouter);
+
+//Configuro mustache
+app.set("views", join(__dirname + "/../views"));
+app.set("view engine", "mustache");
+app.engine("mustache", mustacheExpress());
+
+//Sirve para permitir a node analizar datos de formularios cuando está en true
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //Sire para que la web pueda usar los archivos de la carpeta public
-app.use(express.static(join(__dirname + '/../public')));
+app.use(express.static(join(__dirname + "/../public")));
 
 //Añado el router
-app.use('/', getRouter);
+app.use("/", teamRouter);
 
-//Directorio actual 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename); 
 
 
 //A PARTIR DE AQUI NO TIENE QUE IR EN ESTA PAGINA
