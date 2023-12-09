5372553e0b7a28755a9b20b9ee2322c3f1a004bb
irenegarccia <117302264+irenegarccia@users.noreply.github.com>
2023-12-09 18:34:27 +0100

    Update servidor.js

--- MixTape-Festival-main/src/servidor.js
+++ MixTape-Festival-main/src/servidor.js
@@ -12,30 +12,79 @@ export function loadSampleData(){
     });
 
     addArtista({
-        nombre: 'BAD BUNNY',
-        imagen: 'FotosArtistas/Bad Bunny.jpg',
-        genero: 'TRAP LATINO'
+        nombre: 'DANI FERNANDEZ',
+        imagen: 'FotosArtistas/Dani Fernandez.jpg',
+        genero: 'POP ESPAÑOL'
     
     });
 
     addArtista({
-        nombre: 'BAD BUNNY',
-        imagen: 'FotosArtistas/Bad Bunny.jpg',
-        genero: 'TRAP LATINO'
+        nombre: 'ESTOPA',
+        imagen: 'FotosArtistas/Estopa.jpg',
+        genero: 'POP ESPAÑOL'
     
     });
 
     addArtista({
-        nombre: 'BAD BUNNY',
-        imagen: 'FotosArtistas/Bad Bunny.jpg',
-        genero: 'TRAP LATINO'
+        nombre: 'HARRY STYLES',
+        imagen: 'FotosArtistas/Harry Styles.jpg',
+        genero: 'POP'
     
     });
 
     addArtista({
-        nombre: 'BAD BUNNY',
-        imagen: 'FotosArtistas/Bad Bunny.jpg',
-        genero: 'TRAP LATINO'
+        nombre: 'KIDD KEO',
+        imagen: 'FotosArtistas/Kidd Keo.jpg',
+        genero: 'TRAP ESPAÑOL'
+    
+    });
+
+    addArtista({
+        nombre: 'MELENDI',
+        imagen: 'FotosArtistas/Melendi.jpg',
+        genero: 'POP ESPAÑOL'
+    
+    });
+
+    addArtista({
+        nombre: 'RAUW ALEJANDRO',
+        imagen: 'FotosArtistas/Rauw Alejandro.jpg',
+        genero: 'REGUETON'
+    
+    });
+
+    addArtista({
+        nombre: 'SHAWN MENDES',
+        imagen: 'FotosArtistas/Shawn Mendes.jpg',
+        genero: 'POP'
+    
+    });
+
+    addArtista({
+        nombre: 'NIKONE',
+        imagen: 'FotosArtistas/Nikone.jpg',
+        genero: 'RAP'
+    
+    });
+
+    addArtista({
+        nombre: 'ONE DIRECTION',
+        imagen: 'FotosArtistas/One Direction.jpg',
+        genero: 'POP'
+    
+    });
+
+    addArtista({
+        nombre: 'QUEVEDO',
+        imagen: 'FotosArtistas/Quevedo.jpg',
+        genero: 'REGUETON'
+    
+    });
+
+    addArtista({
+        nombre: 'TAYLOR SWIFT',
+        imagen: 'FotosArtistas/Taylor Swift.jpg',
+        genero: 'POP'
     
     });
 }
