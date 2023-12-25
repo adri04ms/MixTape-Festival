203c6afc31616a0f966957c3d58cc79bf4f14681
Deliaml10 <114805861+Deliaml10@users.noreply.github.com>
2023-12-25 23:46:04 +0100

    funciones de la barra de busqueda

--- public/app.js
+++ public/app.js
@@ -17,3 +17,28 @@ async function loadMore() {
         console.error('Error al cargar artistas:', error);
     }
 }
+
+
+function search() {
+    var input = document.getElementById('searchInput').value.trim().toLowerCase();
+    var artistas = document.getElementsByClassName('cantante');
+
+    for (var i = 0; i < artistas.length; i++) {
+        var nombre = artistas[i].getElementsByTagName('p')[0].innerText.trim().toLowerCase();
+        var genero = artistas[i].getElementsByTagName('h3')[0].innerText.trim().toLowerCase();
+
+        // Mostrar u ocultar el elemento según si coincide con la búsqueda en nombre o género
+        if (nombre.includes(input) || genero.includes(input)) {
+            artistas[i].style.display = 'block';
+        } else {
+            artistas[i].style.display = 'none';
+        }
+    }
+}
+function resetSearch() {
+    var artistas = document.getElementsByClassName('cantante');
+
+    for (var i = 0; i < artistas.length; i++) {
+        artistas[i].style.display = 'block';
+    }
+}
